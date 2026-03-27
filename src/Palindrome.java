d99ec470b3d2a3be112797261b2d241ea7e351b7
Dheirya <108139056+DheiryaMehta10@users.noreply.github.com>
2026-03-27 21:53:32 +0530

    Create Palindrome.java

---
+++ src/Palindrome.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Palindrome_Checker_App {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+        // Display application version
+        System.out.println("Version : 1.0");
+
+        // Display initialization message
+        System.out.println("System initialized successfully.");
+    }
+}
